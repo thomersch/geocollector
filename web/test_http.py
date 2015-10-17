b1772db4b63cc094eb8a7c74dc785cafb5e4fd09
Thomas Skowron <th@skowron.biz>
2015-10-17 17:04:33 +0200

    added first test for web api

---
+++ web/test_http.py
@@ -0,0 +1,13 @@
+import http
+
+import pytest
+
+@pytest.fixture
+def app():
+	http.app.config["TESTING"] = True
+	return http.app.test_client()
+
+class TestAPI:
+	def test_index(self):
+		response = app().get("/")
+		assert response.status == "200 OK"
